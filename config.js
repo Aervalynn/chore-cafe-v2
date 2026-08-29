666e6b50ff67cbc175d4aaa35260abc61c8e71f7
Aervalynn <this.is.jay.harris@gmail.com>
2026-08-29 02:06:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 const SUPABASE_URL = "https://dvvcgewfmpivzikokdea.supabase.co";
 const SUPABASE_KEY = "sb_publishable_TKIREc13KwUJMs8Owtq09w_nNFV6pVj";
+const ONESIGNAL_APP_ID = "eb329c26-b880-4b2d-aba2-eb0f0f81f5e6";
